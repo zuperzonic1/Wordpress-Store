94713ca5b4e6244b50b8950e12915f2f1877c358
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-02-13 16:13:02 -0500

    Add Google Fonts to store theme styles

--- themes/store-theme/functions.php
+++ themes/store-theme/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions and definitions
  *
@@ -15,12 +16,21 @@
  *
  * @return void
  */
-function store_theme_styles() {
+function store_theme_styles()
+{
 	wp_enqueue_style(
 		'store-theme-style',
 		get_stylesheet_uri(),
 		[],
-		wp_get_theme()->get( 'Version' )
+		wp_get_theme()->get('Version')
 	);
 }
-add_action( 'wp_enqueue_scripts', 'store_theme_styles' );
+
+function mytheme_enqueue_google_fonts()
+{
+	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Istok+Web:ital,wght@0,400;0,700;1,400;1,700&family=Jaldi:wght@400;700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap', false);
+}
+
+
+
+add_action('wp_enqueue_scripts', 'store_theme_styles', 'mytheme_enqueue_google_fonts');
